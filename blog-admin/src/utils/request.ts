44c6585a58be92e39f5becb58dda8213a184101f
quequnlong <1248954763@qq.com>
2025-02-26 08:46:12 +0800

    优化

--- blog-admin/src/utils/request.ts
+++ blog-admin/src/utils/request.ts
@@ -28,9 +28,9 @@ service.interceptors.response.use(
   (response) => {
     const res = response.data
     // 二进制数据则直接返回
-    if (res.request.responseType ===  'blob' || res.request.responseType ===  'arraybuffer') {
-      return res.data
-    }
+    // if (res.request.responseType ===  'blob' || res.request.responseType ===  'arraybuffer') {
+    //   return res.data
+    // }
     if (res.code !== 200) {
       ElMessage.error(res.message || '请求错误')
       if (res.code === 401) {
